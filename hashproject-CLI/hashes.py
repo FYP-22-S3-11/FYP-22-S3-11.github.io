7eac30efa557231b669482a10e135e49e7d958d0
chromanite <26741875+chromanite@users.noreply.github.com>
2022-09-17 15:57:28 +0800

    fix: code refactoring

--- hashproject-CLI/hashes.py
+++ hashproject-CLI/hashes.py
@@ -8,8 +8,6 @@ import argparse
 import requests
 from collections import namedtuple
 
-import requests
-
 Prototype = namedtuple('Prototype', ['regex', 'modes'])
 HashInfo = namedtuple('HashInfo', ['name'])
 
@@ -17,10 +15,10 @@ prototypes = [
     Prototype(
         regex=re.compile(r'^[a-f0-9]{4}$', re.IGNORECASE),
         modes=[
-            HashInfo(name='CRC-16'), 
+            HashInfo(name='CRC-16'),
             HashInfo(name='CRC-16-CCITT'),
             HashInfo(name='FCS-16')]),
-    
+
     Prototype(
         regex=re.compile(r'^[a-f0-9]{8}$', re.IGNORECASE),
         modes=[
@@ -38,13 +36,13 @@ prototypes = [
     Prototype(
         regex=re.compile(r'^[a-f0-9]{6}$', re.IGNORECASE),
         modes=[HashInfo(name='CRC-24')]),
-    
+
     Prototype(
         regex=re.compile(r'^(\$crc32\$[a-f0-9]{8}.)?[a-f0-9]{8}$', re.IGNORECASE),
         modes=[
             HashInfo(name='CRC-32')]),
     Prototype(
-        regex=re.compile(r'^[a-z0-9\/.]{13}$', re.IGNORECASE),
+        regex=re.compile(r'^[a-z0-9/.]{13}$', re.IGNORECASE),
         modes=[
             HashInfo(name='DES(Unix)'),
             HashInfo(name='Traditional DES'),
@@ -55,7 +53,7 @@ prototypes = [
             HashInfo(name='FNV-164'),
             HashInfo(name='CRC-64')]),
     Prototype(
-        regex=re.compile(r'^_[a-z0-9\/.]{19}$', re.IGNORECASE),
+        regex=re.compile(r'^_[a-z0-9/.]{19}$', re.IGNORECASE),
         modes=[
             HashInfo(name='BSDi Crypt')]),
     Prototype(
@@ -63,7 +61,7 @@ prototypes = [
         modes=[
             HashInfo(name='CRC-96(ZIP)')]),
     Prototype(
-        regex=re.compile(r'^[a-z0-9\/.]{24}$', re.IGNORECASE),
+        regex=re.compile(r'^[a-z0-9/.]{24}$', re.IGNORECASE),
         modes=[
             HashInfo(name='Crypt16')]),
     Prototype(
@@ -82,10 +80,6 @@ prototypes = [
             HashInfo(name='Tiger-128', ),
             HashInfo(name='Skein-256(128)', ),
             HashInfo(name='Skein-512(128)', ),
-            HashInfo(name='Lotus Notes/Domino 5'),
-            HashInfo(name='Skype'),
-            HashInfo(name='ZipMonster'),
-            HashInfo(name='PrestaShop'),
             HashInfo(name='md5(md5(md5($pass)))'),
             HashInfo(name='md5(strtoupper(md5($pass)))'),
             HashInfo(name='md5(sha1($pass))'),
@@ -96,12 +90,12 @@ prototypes = [
         modes=[
             HashInfo(name='Snefru-128')]),
     Prototype(
-        regex=re.compile(r'^{SHA}[a-z0-9\/+]{27}=$', re.IGNORECASE),
+        regex=re.compile(r'^{SHA}[a-z0-9/+]{27}=$', re.IGNORECASE),
         modes=[
             HashInfo(name='SHA-1(Base64)'),
             HashInfo(name='Netscape LDAP SHA')]),
     Prototype(
-        regex=re.compile(r'^\$1\$[a-z0-9\/.]{0,8}\$[a-z0-9\/.]{22}(:.*)?$', re.IGNORECASE),
+        regex=re.compile(r'^\$1\$[a-z0-9/.]{0,8}\$[a-z0-9/.]{22}(:.*)?$', re.IGNORECASE),
         modes=[
             HashInfo(name='MD5 Crypt'),
             HashInfo(name='FreeBSD MD5')]),
@@ -110,13 +104,13 @@ prototypes = [
         modes=[
             HashInfo(name='Lineage II C4')]),
     Prototype(
-        regex=re.compile(r'^\$apr1\$[a-z0-9\/.]{0,8}\$[a-z0-9\/.]{22}$', re.IGNORECASE),
+        regex=re.compile(r'^\$apr1\$[a-z0-9/.]{0,8}\$[a-z0-9/.]{22}$', re.IGNORECASE),
         modes=[
             HashInfo(name='MD5(APR)'),
             HashInfo(name='Apache MD5'),
             HashInfo(name='md5apr1')]),
     Prototype(
-        regex=re.compile(r'^{smd5}[a-z0-9$\/.]{31}$', re.IGNORECASE),
+        regex=re.compile(r'^{smd5}[a-z0-9$/.]{31}$', re.IGNORECASE),
         modes=[
             HashInfo(name='AIX(smd5)')]),
     Prototype(
@@ -148,11 +142,13 @@ prototypes = [
         modes=[
             HashInfo(name='CryptoCurrency(PrivateKey)')]),
     Prototype(
-        regex=re.compile(r'^{ssha1}[\d]{2}\$[a-z0-9$\/.]{44}$', re.IGNORECASE),
+        regex=re.compile(r'^{ssha1}\d{2}\$[a-z0-9$/.]{44}$', re.IGNORECASE),
         modes=[
             HashInfo(name='AIX(ssha1)')]),
     Prototype(
-        regex=re.compile(r'^(\$md5,rounds=[\d]+\$|\$md5\$rounds=[\d]+\$|\$md5\$)[a-z0-9\/.]{0,16}(\$|\$\$)[a-z0-9\/.]{22}$', re.IGNORECASE),
+        regex=re.compile(
+            r'^(\$md5,rounds=\d+\$|\$md5\$rounds=\d+\$|\$md5\$)[a-z0-9/.]{0,16}(\$|\$\$)[a-z0-9/.]{22}$',
+            re.IGNORECASE),
         modes=[
             HashInfo(name='Sun MD5 Crypt')]),
     Prototype(
@@ -164,13 +160,12 @@ prototypes = [
             HashInfo(name='Skein-256(224)', ),
             HashInfo(name='Skein-512(224)')]),
     Prototype(
-        regex=re.compile(r'^(\$2[axy]|\$2)\$[\d]{2}\$[a-z0-9\/.]{53}$', re.IGNORECASE),
+        regex=re.compile(r'^(\$2[axy]|\$2)\$\d{2}\$[a-z0-9/.]{53}$', re.IGNORECASE),
         modes=[
             HashInfo(name='Blowfish(OpenBSD)'),
-            HashInfo(name='Woltlab Burning Board 4.x'),
             HashInfo(name='bcrypt')]),
     Prototype(
-        regex=re.compile(r'^\$bcrypt-sha256\$(2[axy]|2)\,[\d]+\$[a-z0-9\/.]{22}\$[a-z0-9\/.]{31}$', re.IGNORECASE),
+        regex=re.compile(r'^\$bcrypt-sha256\$(2[axy]|2),\d+\$[a-z0-9/.]{22}\$[a-z0-9/.]{31}$', re.IGNORECASE),
         modes=[
             HashInfo(name='bcrypt(SHA-256)')]),
     Prototype(
@@ -189,14 +184,14 @@ prototypes = [
             HashInfo(name='Skein-256'),
             HashInfo(name='Skein-512(256)', ),
             HashInfo(name='Ventrilo'),
-            HashInfo(name='HMAC-SHA256 (key = $pass)'),]),
+            HashInfo(name='HMAC-SHA256 (key = $pass)'), ]),
     Prototype(
-        regex=re.compile(r'^(\$chap\$0\*)?[a-f0-9]{32}[\*:][a-f0-9]{32}(:[\d]{2})?$', re.IGNORECASE),
+        regex=re.compile(r'^(\$chap\$0\*)?[a-f0-9]{32}[*:][a-f0-9]{32}(:\d{2})?$', re.IGNORECASE),
         modes=[
             HashInfo(name='MD5(Chap)'),
             HashInfo(name='iSCSI CHAP Authentication')]),
     Prototype(
-        regex=re.compile(r'^{ssha256}[\d]{2}\$[a-z0-9$\/.]{60}$', re.IGNORECASE),
+        regex=re.compile(r'^{ssha256}\d{2}\$[a-z0-9$/.]{60}$', re.IGNORECASE),
         modes=[
             HashInfo(name='AIX(ssha256)')]),
     Prototype(
@@ -207,16 +202,16 @@ prototypes = [
         regex=re.compile(r'^[a-f0-9]{96}$', re.IGNORECASE),
         modes=[
             HashInfo(name='SHA-384'),
-            HashInfo(name='SHA3-384',),
-            HashInfo(name='Skein-512(384)',),
-            HashInfo(name='Skein-1024(384)',)]),
+            HashInfo(name='SHA3-384', ),
+            HashInfo(name='Skein-512(384)', ),
+            HashInfo(name='Skein-1024(384)', )]),
     Prototype(
-        regex=re.compile(r'^{SSHA512}[a-z0-9\/+]{96}$', re.IGNORECASE),
+        regex=re.compile(r'^{SSHA512}[a-z0-9/+]{96}$', re.IGNORECASE),
         modes=[
             HashInfo(name='SSHA-512(Base64)'),
             HashInfo(name='LDAP(SSHA-512)')]),
     Prototype(
-        regex=re.compile(r'^{ssha512}[\d]{2}\$[a-z0-9\/.]{16,48}\$[a-z0-9\/.]{86}$', re.IGNORECASE),
+        regex=re.compile(r'^{ssha512}\d{2}\$[a-z0-9/.]{16,48}\$[a-z0-9/.]{86}$', re.IGNORECASE),
         modes=[
             HashInfo(name='AIX(ssha512)')]),
     Prototype(
@@ -224,18 +219,18 @@ prototypes = [
         modes=[
             HashInfo(name='SHA-512'),
             HashInfo(name='Whirlpool'),
-            HashInfo(name='Salsa10',),
-            HashInfo(name='Salsa20',),
+            HashInfo(name='Salsa10', ),
+            HashInfo(name='Salsa20', ),
             HashInfo(name='SHA3-512'),
             HashInfo(name='Skein-512'),
             HashInfo(name='Skein-1024(512)', ),
-            HashInfo(name='HMAC-SHA512 (key = $pass)'),]),
+            HashInfo(name='HMAC-SHA512 (key = $pass)'), ]),
     Prototype(
         regex=re.compile(r'^[a-f0-9]{256}$', re.IGNORECASE),
         modes=[
             HashInfo(name='Skein-1024')]),
     Prototype(
-        regex=re.compile(r'^\$5\$(rounds=[\d]+\$)?[a-z0-9\/.]{0,16}\$[a-z0-9\/.]{43}$', re.IGNORECASE),
+        regex=re.compile(r'^\$5\$(rounds=\d+\$)?[a-z0-9/.]{0,16}\$[a-z0-9/.]{43}$', re.IGNORECASE),
         modes=[
             HashInfo(name='SHA-256 Crypt')]),
     Prototype(
@@ -247,27 +242,27 @@ prototypes = [
         modes=[
             HashInfo(name='IPMI2 RAKP HMAC-SHA1')]),
     Prototype(
-        regex=re.compile(r'^\$sha1\$[\d]+\$[a-z0-9\/.]{0,64}\$[a-z0-9\/.]{28}$', re.IGNORECASE),
+        regex=re.compile(r'^\$sha1\$\d+\$[a-z0-9/.]{0,64}\$[a-z0-9/.]{28}$', re.IGNORECASE),
         modes=[
             HashInfo(name='SHA-1 Crypt')]),
     Prototype(
-        regex=re.compile(r'^\$pbkdf2(-sha1)?\$[\d]+\$[a-z0-9\/.]+\$[a-z0-9\/.]{27}$', re.IGNORECASE),
+        regex=re.compile(r'^\$pbkdf2(-sha1)?\$\d+\$[a-z0-9/.]+\$[a-z0-9/.]{27}$', re.IGNORECASE),
         modes=[
             HashInfo(name='PBKDF2-SHA1(Generic)')]),
     Prototype(
-        regex=re.compile(r'^\$pbkdf2-sha256\$[\d]+\$[a-z0-9\/.]+\$[a-z0-9\/.]{43}$', re.IGNORECASE),
+        regex=re.compile(r'^\$pbkdf2-sha256\$\d+\$[a-z0-9/.]+\$[a-z0-9/.]{43}$', re.IGNORECASE),
         modes=[
             HashInfo(name='PBKDF2-SHA256(Generic)')]),
     Prototype(
-        regex=re.compile(r'^\$pbkdf2-sha512\$[\d]+\$[a-z0-9\/.]+\$[a-z0-9\/.]{86}$', re.IGNORECASE),
+        regex=re.compile(r'^\$pbkdf2-sha512\$\d+\$[a-z0-9/.]+\$[a-z0-9/.]{86}$', re.IGNORECASE),
         modes=[
             HashInfo(name='PBKDF2-SHA512(Generic)')]),
     Prototype(
-        regex=re.compile(r'^\$p5k2\$[\d]+\$[a-z0-9\/+=-]+\$[a-z0-9\/+-]{27}=$', re.IGNORECASE),
+        regex=re.compile(r'^\$p5k2\$\d+\$[a-z0-9/+=-]+\$[a-z0-9/+-]{27}=$', re.IGNORECASE),
         modes=[
             HashInfo(name='PBKDF2(Cryptacular)')]),
     Prototype(
-        regex=re.compile(r'^\$p5k2\$[\d]+\$[a-z0-9\/.]+\$[a-z0-9\/.]{32}$', re.IGNORECASE),
+        regex=re.compile(r'^\$p5k2\$\d+\$[a-z0-9/.]+\$[a-z0-9/.]{32}$', re.IGNORECASE),
         modes=[
             HashInfo(name='PBKDF2(Dwayne Litzenberger)')]),
     Prototype(
@@ -279,11 +274,11 @@ prototypes = [
         modes=[
             HashInfo(name='IKE-PSK SHA1')]),
     Prototype(
-        regex=re.compile(r'^SCRYPT:[\d]+:[\d]{1}:[\d]{1}:[a-z0-9:\/+=]+$', re.IGNORECASE),
+        regex=re.compile(r'^SCRYPT:\d+:\d:\d:[a-z0-9:/+=]+$', re.IGNORECASE),
         modes=[
             HashInfo(name='scrypt')]),
     Prototype(
-        regex=re.compile(r'^[a-z0-9\/.]{13,}$', re.IGNORECASE),
+        regex=re.compile(r'^[a-z0-9/.]{13,}$', re.IGNORECASE),
         modes=[
             HashInfo(name='BigCrypt')]),
 ]
@@ -294,12 +289,11 @@ class HashID(object):
     HashID with configurable prototypes
     """
 
-    def __init__(self, prototypes=prototypes):
+    def __init__(self, lprototypes=prototypes):
         super(HashID, self).__init__()
 
-        # Set self.prototypes to a copy of prototypes to allow
-        # modification after instantiation
-        self.prototypes = list(prototypes)
+        # Set self.prototypes to a copy of prototypes to allow modification after instantiation
+        self.prototypes = list(lprototypes)
 
     def identify_hash(self, phash):
         """
@@ -312,9 +306,9 @@ class HashID(object):
                     yield mode
 
 
-def write_result(identified_modes, outfile, sysout = None):
+def write_result(identified_modes, outfile):
     """
-    Write human readable output from identify_hash
+    Write human-readable output from identify_hash
     """
     count = 0
     hashTypes = ""
@@ -324,7 +318,7 @@ def write_result(identified_modes, outfile, sysout = None):
     outfile.write(hashTypes + "\n")
     if count == 0:
         outfile.write(f"[+] Unknown hash\n")
-    return (count > 0)
+    return count > 0
 
 
 def main():
@@ -340,10 +334,11 @@ def main():
                         metavar="INPUT", type=str, nargs="*",
                         help="input to analyze (default: STDIN)")
     group = parser.add_argument_group('options')
+    
     group.add_argument("-o", "--outfile",
                        metavar="FILE", type=str,
                        help="Write output to file")
-    
+
     group.add_argument("-c", "--coin",
                        metavar="COIN", type=str,
                        help="Type of cryptocurrency coin to search for")
@@ -353,7 +348,6 @@ def main():
                        help="Show this help message and exit")
 
     args = parser.parse_args()
-
     hashID = HashID()
 
     if not args.outfile:
@@ -363,7 +357,6 @@ def main():
             outfile = io.open(args.outfile, "w", encoding="utf-8")
         except EnvironmentError:
             parser.error(f"Could not open {args.output}")
-
 
     if not args.strings or args.strings[0] == "-":
         if args.coin:
